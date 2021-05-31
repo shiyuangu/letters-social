f2f7899d69309fc7517c623040d9b174adc02bfd
shiyuangu <shiyuangu@gmail.com>
2021-05-31 13:20:01 -0700

    Update Link.js

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -10,6 +10,8 @@ import { navigate } from '../../history';
  */
 function Link(props) {
     const { to, children } = props;
+    //sgu: Children.only verifies only one children; 
+    //cloneElement modifies this child (since we cannot change the child directly) 
     return cloneElement(Children.only(children), {
         onClick: () => navigate(to)
     });
